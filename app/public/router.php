fdea3fe646aac42a7d08a389009b0647dcdc1262
zhlix <15127441165@sina.cn>
2019-12-31 16:15:30 +0800

    修改

--- app/public/router.php
+++ app/public/router.php
@@ -2,7 +2,7 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006~2019 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
@@ -13,8 +13,7 @@
 if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"])) {
     return false;
 } else {
-    if (!isset($_SERVER['PATH_INFO'])) {
-        $_SERVER['PATH_INFO'] = $_SERVER['REQUEST_URI'];
-    }
+    $_SERVER["SCRIPT_FILENAME"] = __DIR__ . '/index.php';
+
     require __DIR__ . "/index.php";
 }
